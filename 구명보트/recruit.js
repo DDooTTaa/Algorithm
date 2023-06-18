9826869eda71928f11532910a4a7c6e9e33f6369
DDooTTaa <dimeon@naver.com>
2023-06-18 14:37:17 +0900

    구명보트 주석 추가

--- 구명보트/recruit.js
+++ 구명보트/recruit.js
@@ -29,9 +29,11 @@ function solution(people, limit) {
  */
 function solution(people, limit) {
     var answer = 0;
+    // 사람들을 몸무게가 낮은 순으로 정렬한다. (높은 순으로 해도 상관 없음)
     people.sort((a, b) => b - a);
     let left = 0;
     let right = people.length - 1;
+    // Left 와 Right 의 포커스를 옮겨가며 배의 수를 카운트한다.
     while(left < right) {
         if(people[left] + people[right] > limit) {
             left++;
@@ -41,6 +43,7 @@ function solution(people, limit) {
         }
         answer++;
     }
+    // left 와 right 가 같을 경우(한 사람만 남을 경우 배가 한 척 더 필요하다.)
     if(left === right) answer++;
     return answer;
 }
