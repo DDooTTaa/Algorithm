1b55d02ea43d0247b0e707e7fc39a20b755e5c95
DDooTTaa <80893198+DDooTTaa@users.noreply.github.com>
2025-08-19 11:29:48 +0900

    [level 1] Title: 카드 뭉치, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -0,0 +1,13 @@
+function solution(cards1, cards2, goal) {
+    var answer = 'Yes';
+    goal.forEach((x,i) => {
+        if(cards1[0] === x) {
+            cards1.shift();
+        } else if(cards2[0] === x) {
+            cards2.shift();
+        } else {
+            answer = "No";            
+        }
+    });
+    return answer;
+}
