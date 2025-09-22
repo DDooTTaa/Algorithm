83378009e080256dba5a1a4a19f4d84d1b06a442
DDooTTaa <80893198+DDooTTaa@users.noreply.github.com>
2025-09-22 16:52:21 +0900

    [level 2] Title: 요격 시스템, Time: 592.14 ms, Memory: 167 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/2/181188. 요격 시스템/요격 시스템.js
@@ -0,0 +1,17 @@
+function solution(targets) {
+    let answer = 1;
+    targets.sort((a, b) => {return a[0] - b[0]});
+
+    let e = targets[0][1];
+
+    for(const i of targets){
+        if(e > i[0]){
+            e = Math.min(i[1], e);
+        }else {
+            answer++;
+            e = i[1];
+        }
+    }
+
+    return answer;
+}
